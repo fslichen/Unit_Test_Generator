112cd164bd15b9b4e47d4a687eb7ca56dd7229a8
chenli <chenli@192.168.43.24>
2017-09-20 14:23:40 +0800

    new demos

--- Unit_Test_Generator/src/test/java/generator/UseCaseGenerator.java
+++ Unit_Test_Generator/src/test/java/generator/UseCaseGenerator.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UseCaseGenerator extends BaseTest {
 	@Test
 	public void testInvokeMethodsUnderBasePackageOfSrcMainJavaAndGetMockedParameterValuesAndReturnValues() throws Exception {
-		new UnitTestGenerator().invokeMethodsUnderBasePackageOfSrcMainJavaAndGetMockedParameterValuesAndReturnValues("evolution.controller", new Predicate<Class<?>>() {
+		Predicate<Class<?>> classFileter = new Predicate<Class<?>>() {
 			@Override
 			public boolean test(Class<?> clazz) {
 				return clazz.getAnnotation(RestController.class) != null;
 			}
-		}, webApplicationContext);
+		};
+		new UnitTestGenerator().invokeMethodsUnderBasePackageOfSrcMainJavaAndGetMockedParameterValuesAndReturnValues("evolution.controller", classFileter, webApplicationContext);
 	}
 }
