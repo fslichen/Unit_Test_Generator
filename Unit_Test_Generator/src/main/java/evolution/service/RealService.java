e9e130f02a3255f45373dfb4ec07c0ffe5d61fc4
chenli <chenli@192.168.43.24>
2017-10-10 22:00:28 +0800

    new demos

--- Unit_Test_Generator/src/main/java/evolution/service/RealService.java
+++ Unit_Test_Generator/src/main/java/evolution/service/RealService.java
@@ -8,6 +8,11 @@ import evolution.pojo.AnyPojo;
 @Service
 public class RealService {
 	public AnotherPojo anyMethod(AnyPojo anyPojo) {
+		anotherMethod(anyPojo);
+		return new AnotherPojo();
+	}
+	
+	public AnotherPojo anotherMethod(AnyPojo anyPojo) {
 		return new AnotherPojo();
 	}
 }
