e0564413ff42c711ad03e558e768d7c5da4c3435
chenli <chenli@192.168.43.24>
2017-10-10 16:12:52 +0800

    new demos

--- Unit_Test_Generator/src/test/java/generator/UseCaseGenerator.java
+++ Unit_Test_Generator/src/test/java/generator/UseCaseGenerator.java
@@ -1,21 +1,22 @@
 package generator;
 
-import java.util.function.Predicate;
-
 import org.junit.Test;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
 public class UseCaseGenerator extends BaseTestCase {
 	@Test
 	public void run() throws Exception {
-		Predicate<Class<?>> classFileter = new Predicate<Class<?>>() {
-			@Override
-			public boolean test(Class<?> clazz) {
-				return clazz.getAnnotation(RestController.class) != null || clazz.getAnnotation(Service.class) != null;
-			}
-		};
 		UnitTestGenerator unitTestGenerator = new UnitTestGenerator();
-		unitTestGenerator.invokeMethodsUnderBasePackageOfSrcMainJavaAndGenerateUseCasesUnderSrcTestJava(Lang.property("use-case-base-package", String.class), classFileter, webApplicationContext);
+		unitTestGenerator.invokeMethodsUnderBasePackageOfSrcMainJavaAndGenerateUseCasesUnderSrcTestJava(
+				Lang.property("use-case-base-package", String.class), 
+				x -> x.isAnnotationPresent(Controller.class)
+				|| x.isAnnotationPresent(RestController.class)
+				|| x.isAnnotationPresent(Service.class)
+				|| x.isAnnotationPresent(Repository.class)
+				|| x.getSimpleName().endsWith("Mapper"), 
+				webApplicationContext);
 	}
 }
